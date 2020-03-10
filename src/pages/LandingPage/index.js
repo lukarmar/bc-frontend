554d63c9872c6954e6681d093f0c854e9b32e36d
Luka <luka.rmar@gmail.com>
2020-03-10 02:48:20 -0300

    layout# finish web layout

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Form } from '@unform/web';
+import {} from 'react-icons/fa';
 
 import Input from '../../Components/Input';
 import InputMask from '../../Components/InpuMask';
@@ -8,70 +9,112 @@ import {
   Content,
   Banner,
   Benefits,
-  ListBenefits,
   BoxForm,
+  ListTwoPesson,
 } from './styles';
 
 export default function LandingPage() {
   return (
     <Container>
       <header>
-        <img src="" alt="logo" />
+        <img
+          src="https://www.bomcupom.com/wp-content/themes/bomcupom-novo/assets/images/bomcupom-marca.png"
+          alt="logo"
+        />
       </header>
       <Content>
         <Banner>
-          <h1>Franquia Bom Cupom</h1>
-          <h3>Mais que a penas cumpons de descontos</h3>
-          <p>
-            A Bom Cupom nasceu em 2017 com o objetivo de fomentar o comércio
-            local. A empresa é franqueadora de um modelo de negócios que
-            viabiliza a entrega de cupons de desconto físicos para estimular as
-            vendas nos bairros.
-          </p>
-          <p>
-            A Bom Cupom pretende transformar seu modelo de negócios atualmente
-            físico em um modelo digital. Os cupons de desconto deixarão de ser
-            entregues no verso das notas fiscais e passarão a ser entregues
-            digitalmente, dentro de um aplicativo.
-          </p>
+          <div>
+            <img
+              src="https://www.bomcupom.com/wp-content/themes/bomcupom-novo/assets/images/smilefacesgreen.png"
+              alt="cupons"
+            />
+            <h1>Franquia Bom Cupom</h1>
+            <h2>Mais que a penas cumpons de descontos</h2>
+            <h3>
+              Faça parte da revolução dos cupons de descontos e adquira já a sua
+              franquia.
+            </h3>
+          </div>
         </Banner>
         <Benefits>
-          <p>
-            Além dos cupons de desconto, a Bom Cupom pretende desenvolver outras
-            funcionalidades, ajudando os estabelecimentos a se relacionar de
-            forma mais profunda com seus clientes.
-          </p>
-          <ListBenefits>
-            <li>
-              Um mesmo estabelecimento oferecer diversos cupons de desconto
-            </li>
-            <li>Pagamento pelo app</li>
-            <li>Cartão fidelidade virtual</li>
-            <li>Descontos relâmpagos em itens estratégicos</li>
-            <li>Geofencing</li>
-            <li>
-              Controle de dados de consumo e histórico dos clientes dos
-              estabelecimentos
-            </li>
-            <li>Pesquisas de NPS do estabelecimento com os usuários</li>
-            <li>
-              Página do estabelecimento personalizada de acordo com suas
-              preferências
-            </li>
-          </ListBenefits>
+          <div>
+            <h1 id="h1-1">Benefícios</h1>
+            <ListTwoPesson>
+              <li>
+                <h1>Descontos</h1>
+                <div>
+                  <span>
+                    Faça sua compra em qualquer um dos nossos Veiculadores
+                    parceiros.
+                  </span>
+                  <span>
+                    Após a compra, guarde a sua notinha com os descontos.
+                  </span>
+                  <span>
+                    Use o desconto no nosso Anunciante parceiro. É só pegar e
+                    usar.
+                  </span>
+                </div>
+              </li>
+              <li>
+                <h1>Franquias </h1>
+                <div>
+                  <span>
+                    Não precisa de ponto comercial. Você pode trabalhar de casa,
+                    sem a necessidade de uma equipe. Com isso, seus custos
+                    operacionais são baixos.
+                  </span>
+                  <span>
+                    Nosso modelo de negócios trata-se de uma micro franquia.
+                    Portanto o investimento é baixo e o payback estimado é de 3
+                    a 7 meses.
+                  </span>
+                  <span>
+                    Por se tratar de uma inovação, a aceitação entre os
+                    estabelecimentos é muito boa e, com isso, a taxa de
+                    conversão nas negociações é alta.
+                  </span>
+                </div>
+              </li>
+            </ListTwoPesson>
+          </div>
         </Benefits>
         <BoxForm>
-          <Form>
-            <Input name="name" label="Nome" placeholder="Nome" />
-            <Input name="email" label="E-mail" placeholder="E-mail" />
-            <Input name="password" label="Senha" placeholder="Senha" />
-            <InputMask
-              name="birthDate"
-              label="Data de nascimento"
-              placeholder="Data de nascimento"
-            />
-            <Input name="sex" label="Sexo" placeholder="Sexo" />
-          </Form>
+          <section>
+            Faça parte da revolução dos cupons de descontos e adquira já a sua
+            franquia.
+            <div>
+              <span>Mais de 120 franquias pelo Brasil</span>
+              <span>
+                Cerca de 93% dos franqueados indicariam a BomCupom pra um amigo
+              </span>
+              <span>Presentes em 21 estados do Brasil</span>
+            </div>
+          </section>
+          <div>
+            <h1>Cadastre-se</h1>
+            <Form>
+              <Input name="name" label="Seu nome" placeholder="Nome Completo" />
+              <Input
+                name="email"
+                label="Seu email para contato"
+                placeholder="exemplo@exemplo.com"
+              />
+              <Input
+                name="password"
+                label="Sua senha"
+                placeholder="**********"
+              />
+              <InputMask
+                name="birthDate"
+                label="Sua data de nascimento"
+                placeholder="Data de nascimento"
+              />
+              <Input name="sex" label="Seu sexo" placeholder="Sexo" />
+              <button type="submit">Registrar</button>
+            </Form>
+          </div>
         </BoxForm>
       </Content>
     </Container>
--- src/pages/LandingPage/styles.js
+++ src/pages/LandingPage/styles.js
@@ -1,8 +1,223 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
-export const Container = styled.div``;
-export const Content = styled.main``;
-export const Banner = styled.div``;
-export const Benefits = styled.div``;
-export const BoxForm = styled.div``;
-export const ListBenefits = styled.ul``;
+export const Container = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+
+  header {
+    width: 100%;
+    height: auto;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #f7f4f9;
+
+    img {
+      height: 36px;
+      width: 215px;
+    }
+  }
+`;
+export const Content = styled.main`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+`;
+export const Banner = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 70px 0 100px 0;
+  height: auto;
+  background: #652581;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 100%;
+    width: 60px;
+    height: 60px;
+    background: #652581;
+    clip-path: polygon(51% 43%, 0 0, 100% 0);
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: #ffff;
+
+    letter-spacing: 2px;
+    text-align: center;
+    max-width: 900px;
+
+    h1 {
+      font-size: 4em;
+      margin: 10px 0 8px 0;
+    }
+    h2 {
+      font-size: 2.5em;
+      margin-bottom: 10px;
+    }
+    h3 {
+      font-size: 2em;
+    }
+  }
+`;
+export const Benefits = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #f7f4f9;
+  padding: 50px;
+
+  > div {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    max-width: 1200px;
+
+    > h1 {
+      color: #652581;
+      letter-spacing: 2px;
+      font-size: 3em;
+    }
+  }
+`;
+export const ListTwoPesson = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin-top: 20px;
+
+  li {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: auto;
+    width: 100%;
+    color: #652581;
+    padding: 12px;
+
+    div {
+      display: flex;
+      height: auto;
+
+      span {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 18px 18px;
+        background: #ffffff;
+        margin-right: 6px;
+        border-radius: 8px;
+        margin-top: 15px;
+        font-size: 16px;
+        transition: ease 0.6s;
+
+        &:hover {
+          color: #ffffff;
+          background: #652581;
+        }
+      }
+    }
+  }
+`;
+
+export const BoxForm = styled.div`
+  display: flex;
+
+  justify-content: center;
+  padding: 50px 30px 50px 30px;
+  height: auto;
+  background: #652581;
+
+  section {
+    width: 50%;
+    text-align: center;
+    font-size: 3em;
+    color: #ffffff;
+    margin-right: 15px;
+
+    div {
+      background: #652581;
+      margin-top: 10px;
+      span {
+        font-size: 0.5em;
+        margin-bottom: 40px;
+        transition: ease 0.5s;
+
+        &:hover {
+          font-size: 0.6em;
+        }
+      }
+    }
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 18px;
+
+    background: #ffffff;
+    border-radius: 4px;
+
+    h1 {
+      color: #652581;
+      margin-bottom: 10px;
+    }
+
+    form {
+      display: flex;
+
+      flex-direction: column;
+
+      label {
+        color: #652581;
+        margin: 10px 0 5px 0;
+      }
+
+      input {
+        background: #ffffff;
+        border: 1px solid #dddddd;
+        height: 45px;
+        padding: 13px 0 13px 15px;
+        width: 300px;
+        border-radius: 4px;
+      }
+
+      button {
+        width: 100%;
+        height: auto;
+        padding: 13px 65px 13px 65px;
+        background: #1aa06f;
+        outline: none;
+        border: 0;
+        margin-top: 8px;
+        border-radius: 4px;
+        color: #ffffff;
+        font-weight: bold;
+        transition: ease 0.4s;
+
+        :hover {
+          background: ${darken(0.05, '#1aa06f')};
+        }
+      }
+    }
+  }
+`;
